459fb76d734bff3e785aaebfe36bd27f4628d29c
Brian Diaz <beerre20@gmail.com>
2023-04-22 23:15:43 -0300

    Arreglando funcion de conteo con var

--- exercises/bR-3929/countTo10Var.js
+++ exercises/bR-3929/countTo10Var.js
@@ -1,10 +1,10 @@
-function main(){
-    for(var i = 1; i <= 10; i = i + 1){
-      function imprimir(i){
-        return function(){
-          console.log(i);
+  function main(){
+    for (var i = 1; i <= 10; i++){
+        function imprimir(i){
+            return function(){
+                console.log(i)
+            }
         }
-      }
-      setTimeOut(imprimir(i), 1000 * i);
+        setTimeout(imprimir(i), 1000 * i)
     }
-  }
+}
