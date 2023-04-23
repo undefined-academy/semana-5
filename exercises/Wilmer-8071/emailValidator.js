096e9d4313ff0e220510f09618b92a3d98160a8d
DevOld112 <XwilmerX53@gmail.com>
2023-04-22 19:18:08 -0500

    Ejercicios 01, 02 y 03

---
+++ exercises/Wilmer-8071/emailValidator.js
@@ -0,0 +1,9 @@
+// Ejercicio 04 - Semana 05: Crear una funcionalidad que lea una lista de correos validos y remueva la cadena de texto suaddresing
+
+const correos = [
+    'john.doe@example.com',
+    'jane_doe+shopping@example.com',
+    'alice.smith+work@example.co.uk',
+    'charlie.brown+newsletter@example.io',
+    'emily.watson+notes@example.net'
+]
