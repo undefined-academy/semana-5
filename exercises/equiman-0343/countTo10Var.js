93d82feefdec2965761a7b032c02d14271b08cc0
equiman <equiman@users.noreply.github.com>
2023-04-25 16:34:45 -0500

    correccion de console.log

--- exercises/equiman-0343/countTo10Var.js
+++ exercises/equiman-0343/countTo10Var.js
@@ -2,8 +2,8 @@ const counter = () => {
     const SEC_IN_MS = 1000
 
     const timer = (number) => {
-        const print = () => console.time(number)
-        setTimeout(print, number * SEC_IN_MS);
+        const print = () => console.log(number)
+        setTimeout(print, number * SEC_IN_MS); l
     }
 
     for (var i = 1; i <= 10; i++) {
