c52c1893d290d8396e40951f3c3dad92bbd8ce61
LizethCs <lcangulo26@gmail.com>
2023-04-28 20:56:52 -0500

    Fix emailValidator.js

--- exercises/Lizeth-9127/emailValidator.js
+++ exercises/Lizeth-9127/emailValidator.js
@@ -1,12 +1,12 @@
 const emailsArray = [
     "john.doe@example.com",
-    "jane_doe+shopping@example.org", //Invalid'
+    "jane_doe+shopping@example.org",
     "alice.smith+work@example.co.uk",
     "charlie.brown+newsletter@example.io",
     "emily.watson+notes@example.net",
     "this@somethig.", //Invalid
   ];
-  const regExp = /^[\w0-9]+(\.[0-9\+\w-]+)*@([\w-]+\.)+[a-zzA-Z]{2,}$/;
+  const regExp = /^[\w]+([\.\+\w]+)?@([\w-]+\.)+[a-zzA-Z]{2,}$/;
   const isValidEmail = email => regExp.test(email);
   const replaceSubaddresing = email => email.replace(/\+\w+/, "");
   const validEmailsWithNoSubaddressing = emailsArray
