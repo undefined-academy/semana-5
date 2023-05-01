b01fa6fbeec231ee58fdf05d4dbb8694f52c907d
diirf <freinva21@gmail.com>
2023-05-02 00:48:04 +0200

    añado ejercicio para validar correos

---
+++ exercises/diirfv-7677/emailValidator.js
@@ -0,0 +1,20 @@
+const emails = [
+    "john.doe@example.com",
+    "jane_doe+shopping@example.org",
+    "alice.smith+work@example.co.uk",
+    "charlie.brown+newsletter@example.io",
+    "emily.watson+notes@example.net"];
+
+const regx = new RegExp("(^[a-zA-Z0-9]+)([\\w\\-\\.]*)(\\+[\\w\\-\\.]+)*@([\\w\\-]+)(\\.[\\.a-zA-Z]{2,})$");
+
+function filterEmails(emailList){
+    let filteredEmails = emailList
+    .filter(eachEmail => eachEmail.match(regx))
+    .map(filteredEmail => filteredEmail
+        .replace(filteredEmail.match(regx)[3],"")
+    );
+
+    return filteredEmails;
+}
+
+console.log(filterEmails(emails));
