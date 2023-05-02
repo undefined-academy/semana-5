6362811572cbfcd5328fa21b83abf76da8a06aab
diirf <freinva21@gmail.com>
2023-05-03 00:08:41 +0200

    añado cambios en la regExp del ejercicio de validación de correos

--- exercises/diirfv-7677/emailValidator.js
+++ exercises/diirfv-7677/emailValidator.js
@@ -3,9 +3,13 @@ const emails = [
     "jane_doe+shopping@example.org",
     "alice.smith+work@example.co.uk",
     "charlie.brown+newsletter@example.io",
-    "emily.watson+notes@example.net"];
+    "emily.watson+notes@example.net",
+    "mail...usuario@mail.com",
+    "usuario.mail@mail....com",
+    "usuario-.@mail.com",
+    "usuario@mail@mail-.com"];
 
-const regx = new RegExp("(^[a-zA-Z0-9]+)([\\w\\-\\.]*)(\\+[\\w\\-\\.]+)*@([\\w\\-]+)(\\.[\\.a-zA-Z]{2,})$");
+const regx = new RegExp("(^[a-zA-Z0-9]+)(\\.?\\-*[\\w]+)*(\\+[\\w\\-\\.]+)*@([\\w\\-]+)(\\.[a-zA-Z]{2,})+$");
 
 function filterEmails(emailList){
     let filteredEmails = emailList
