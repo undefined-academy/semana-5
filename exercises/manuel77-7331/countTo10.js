b789abab51ef92e59e2c97bcf86eefe01072c69e
manuel <manuelmannuel@gmail.com>
2023-05-03 21:15:30 -0600

    fix: countTo10

--- exercises/manuel77-7331/countTo10.js
+++ exercises/manuel77-7331/countTo10.js
@@ -1,5 +1,5 @@
 function count_num() {
-    for (var num = 0; num <= 10; num++) {
+    for (let num = 0; num <= 10; num++) {
         setTimeout(() => {
             console.log(num);
         }, 1000 * num);
