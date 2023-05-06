9636defc9cdd1f5644f3af027cbdc0011ccd6434
luismdev <morais.luism.dev@gmail.com>
2023-05-06 06:12:59 +0200

    JavaScript_3_06.05.2023

--- exercises/luismdev-0785/countTo10Var.js
+++ exercises/luismdev-0785/countTo10Var.js
@@ -2,10 +2,13 @@ const ONE_SECOND_IN_MS= 1000
     
 function imprimirNumeros(){
     for (var i = 1; i <= 10; i++) {
-        function print(){
-            console.log(i);
+        function print(i){
+            return function(){
+                console.log(i);
+            }
+            
         }
-        setTimeout(print, ONE_SECOND_IN_MS * i);
+        setTimeout(print(i), ONE_SECOND_IN_MS * i);
     }
 }
 
