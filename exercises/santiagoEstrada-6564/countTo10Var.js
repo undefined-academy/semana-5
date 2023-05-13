836afefd5082618a7122326ebda05738be0e9e13
santy01 <santiago01est@gmail.com>
2023-05-13 13:06:41 -0500

    Semana-5 ejericio cuenta de 1 a 10 con var e implementacion de closure, un numero cada segundo

---
+++ exercises/santiagoEstrada-6564/countTo10Var.js
@@ -0,0 +1,16 @@
+// Hacer una función que imprima los números del 1 al 10, 
+// un número cada segundo, pero usando un ciclo for con un var.
+// Ejercicio con var en el for e implementación del closure
+const ONE_SEC_IN_MS=1000;
+
+function main(){
+  for(var i=1; i<=10;i++){
+    function imprimir(i){
+      return function(){
+          console.log(i)
+      }
+    }
+    setTimeout(imprimir(i), ONE_SEC_IN_MS*i)
+  }
+}
+main()
