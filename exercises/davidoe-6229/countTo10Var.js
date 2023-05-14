c3f67dcc7db1911453af59d36f28e5d36aec9cf6
DAVOE1 <imtcdavidochoa@gmail.com>
2023-05-13 21:57:11 -0600

    Ejercicio tres semana 5

---
+++ exercises/davidoe-6229/countTo10Var.js
@@ -0,0 +1,18 @@
+const ONE_SEC_IN_MS = 1000;
+
+function main() {
+    for (var i = 1; i <= 10; i++) {
+        function print(i) {
+            return function () {
+                console.log(i)
+
+            }
+        }
+
+        setTimeout(print(i), ONE_SEC_IN_MS * i)
+    }
+
+}
+
+
+main()
