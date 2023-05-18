fe5936d56238ca5c7d4c5c47c5639c00de453dde
Ezequiel Castellanos <51804994+EzequielCaste@users.noreply.github.com>
2023-05-17 21:12:39 -0300

    exercise Class 3 Week 5

---
+++ exercises/EzequielCaste-9004/CountWithVar.js
@@ -0,0 +1,17 @@
+/**
+ * Ejercicio
+Hacer una función que imprima los números
+del 1 al 10, un número cada segundo, pero
+usando un ciclo for con un var .
+ */
+
+const ONE_SEC_IN_MS = 1000;
+
+function printNumbers() {
+  for (var i = 1; i <= 10; i++) {
+    function imprimir() { console.log(i) }
+    setTimeout(imprimir, i * ONE_SEC_IN_MS);
+  }
+}
+
+printNumbers();
