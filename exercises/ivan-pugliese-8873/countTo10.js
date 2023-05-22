b7e451b4c9106feb4d4b7f9f7a376b613a2d8e65
Ivan Pugliese <pugliese@Pugliese>
2023-05-22 12:39:54 -0300

    corrigiendo ejercicio clase 2

--- exercises/ivan-pugliese-8873/countTo10.js
+++ exercises/ivan-pugliese-8873/countTo10.js
@@ -1,10 +1,11 @@
 const ONE_SEC_IN_MS = 1000;
 
 function main() {
-    function imprimir(i) {
-        console.log(i)
-    }
+   
     for(let i = 1; i <= 10; i++) {
+       function imprimir() {
+        console.log(i)
+      }
         setTimeout(imprimir, ONE_SEC_IN_MS * i);
     }
 }
