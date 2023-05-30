2d0aee9de60be9c06517a0391ee1ac76b51cad6c
Eduardo <ejrb1234@gmail.com>
2023-05-30 17:34:11 -0400

    Adding Guillermo's answer

--- exercises/MrRedu-7518/countTo10Var.js
+++ exercises/MrRedu-7518/countTo10Var.js
@@ -19,4 +19,22 @@ function increment() {
     }
 }
 increment()
-  
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - */ 
+// Versión de Guille:
+
+// let ONE_SEC_IN_MS = 1000;
+function main() {
+  for (var i = 1; i <= 10; i++) {
+
+    function print(i){
+      return function() {
+        console.log(i)
+      }
+    }
+
+    setTimeout(print(i), ONE_SEC_IN_MS * i)
+  }
+}
+// main()
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - */ 
