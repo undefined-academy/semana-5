e955698289e350c18ed285575e5d96c44f168526
PabloXD <42077537+pablo-alex@users.noreply.github.com>
2023-06-04 01:16:33 -0400

    Update emailValidator.js

--- exercises/PabloXD-4695/emailValidator.js
+++ exercises/PabloXD-4695/emailValidator.js
@@ -5,7 +5,7 @@ const email2 = "jane_doe+shopping@example.org";
 const email3 = "alice.smith+work@example.co.uk";
 const email4 = "charlie.brown+newsletter@example.io";
 const email5 = "emily.watson+notes@example.netconst";
-const email6 = "pablo%sds@gamil.com"; //This one should be filtered
+const email6 = "pablo%sds@gamil.com";
 
 const listEmail = [email1, email2, email3, email4, email5, email6];
 
