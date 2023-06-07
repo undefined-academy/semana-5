2602dcfc20f1528ba6212610719572592c7ac3bb
julian <juliansanvill@hotmail.com>
2023-06-07 14:05:30 -0500

    arreglando los problemas vistos

--- exercises/dhdarken-5806/student.js
+++ exercises/dhdarken-5806/student.js
@@ -1,25 +1,26 @@
-class student {
+class Student {
   #name;
   #lastname;
-  construsctor(name, lastname) {
+  constructor(name, lastname) {
     this.#name = name;
     this.#lastname = lastname;
   }
-  get FullName() {
-    return `${this.#name}${this.#lastname} `;
+  get fullname() {
+    return `${this.#name} ${this.#lastname} `;
   }
   static fromObject(obj) {
     const { name, lastname } = obj;
-    return new student(name, lastname);
+    return new Student(name, lastname);
   }
 }
 const studentsInfo = [
-  { name: "ana", lastname: "garcia", scores: "3.5,4,4.5" },
-  { name: "luis", lastname: "perez", scores: "4.5,4,4.5" },
+  { name: "ana", lastname: "garcia", scores: [3, 5, 4, 4.5] },
+  { name: "luis", lastname: "perez", scores: [4.5, 4, 4, 4.5] },
 ];
+
 function calculateAverage(students) {
   const averages = students.map(({ name, lastname, scores }) => {
-    const student = student.fromObject({ name, lastname });
+    const student = Student.fromObject({ name, lastname });
     const scoresSum = scores.reduce((acum, score) => acum + score);
     const average = scoresSum / scores.length;
     return {
@@ -27,6 +28,7 @@ function calculateAverage(students) {
       average: average?.toFixed(2) ?? "N/A",
     };
   });
+
   averages.forEach(({ fullname, average }) => {
     console.log(`${fullname}:${average} `);
   });
