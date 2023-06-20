813e5e83a22758b336685ea8c73b6bfce865565b
StephanyCS1 <stephanycastrosalas@gmail.com>
2023-06-20 13:24:14 -0500

    Ejercicios semana 5

--- exercises/TefisCS96/countTo10.js
+++ exercises/TefisCS96/countTo10.js
@@ -1,11 +1,11 @@
 //Hacer una función que imprima los números del 1 al 10, un número cada segundo.
-const oneSeg = 1000;
+const ONESEG = 1000;
 
 function numberCount(num) {
     for (let i = 1; i <= num; i++) {
         setTimeout(() => {
             console.log(i);
-        }, ( i+1) * oneSeg);
+        }, ( i+1) * ONESEG);
     }
 }
 
--- exercises/TefisCS96/countTo10Var.js
+++ exercises/TefisCS96/countTo10Var.js
@@ -1,12 +1,12 @@
 //Hacer una función que imprima los números del 1 al 10, un número cada segundo, pero usando un ciclo for con un var.
 
-const oneSeg = 1000;
+const ONESEG = 1000;
 function numberCount(num) {
     for (var i = 1; i <= num; i++) {
         let count = i;
         setTimeout(() => {
             console.log(count);
-        }, (i+1) * oneSeg, i);
+        }, (i+1) * ONESEG, i);
     }
 }
 
