9977119524c99408fd7948a6b66a73d04d3e01ef
QuirogaPau <paulaquiroga1207@gmail.com>
2023-07-19 15:34:50 -0300

    Se inicializa el count del metodo reduce en 0, para que llegue a un promedio mas exacto

--- exercises/PaulaQuiroga-8406/student.js
+++ exercises/PaulaQuiroga-8406/student.js
@@ -1,11 +1,12 @@
-// 1. Crea una clase Estudiante con campos privados para el nombre y apellido, un getter para el nombre completo y un método estático fromObject. -
+ // 1. Crea una clase Estudiante con campos privados para el nombre y apellido, un getter para el nombre completo y un método estático fromObject. -
 // 2. Crea una función asíncrona calcularPromedios que acepte un array de estudiantes.
 // 3. Dentro de calcularPromedios, calcula el promedio de las calificaciones al cuadrado de cada estudiante.
 // 4. Imprime el nombre completo y el promedio de calificaciones de cada estudiante utilizando Template literals.
-
 const estudentInfo = [
     { name: 'Ana', lastname: 'García', scores: [3, 5, 4, 4.5] },
-    { name: 'Luis', lastname: 'Pérez', scores: [4.5, 4, 4, 5] }
+    { name: 'Luis', lastname: 'Pérez', scores: [4.5, 4, 4, 5] },
+    { name: 'Irene', lastname: 'Latorre', scores: [2, 3, 5, 5] },
+    { name: 'Esteban', lastname: 'Gomez', scores: [3, 3, 5, 4] },
   ];
 
   class Student {
@@ -32,11 +33,11 @@ const estudentInfo = [
         })
         
         // [3, 5, 4, 4.5]
+        
         const scoresSum = scores.reduce((acum, score) => 
-        acum + score **2 )
+        acum + score **2, 0)
 
         const average = scoresSum / scores.length;
-
         return {
           fullName: student.fullName,
           average: average?.toFixed(2) ?? "N/A"
@@ -56,3 +57,5 @@ const estudentInfo = [
 
   const luis = new Student("Luis", "Pérez");
   // console.log(luis.fullName);
+  const agustin = new Student("Irene", "Latorre");
+  const esteban = new Student("Esteban", "Gomez");
